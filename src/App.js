e3f7d0901b48e74058594835d3978c2dca5acc08
sadeepa bhashitha <73308863+hbsbashitha@users.noreply.github.com>
2021-06-18 13:26:46 +0530

    products add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Appbar from './Components/navbar/topnavbar';
+import Products from './Components/Products/Products';
 function App() {
   return (
     <Router>
       <div className="App">
         <Appbar/>
-        
+        <Products/>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
