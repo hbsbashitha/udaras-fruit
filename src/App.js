e8e4979ce6d3239b13e8a41baa2376dba42f181b
sadeepa bhashitha <73308863+hbsbashitha@users.noreply.github.com>
2021-06-20 00:04:00 +0530

    first step

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Appbar from './Components/navbar/topnavbar';
-import Products from './Components/Products/Products';
-function App() {
+import {Products , Navbar} from './Components'
+import {commerce} from './lib/commerce';
+
+
+const App = () => {
+  const [products, setProducts]=useState([]);
+  const fetchProducts= async() => {
+    const {data}=await commerce.products.list();
+    setProducts(data);
+  }
+
+  useEffect(()=>{
+    fetchProducts();
+  },[]);
+  
+
   return (
     <Router>
       <div className="App">
-        <Appbar/>
-        <Products/>
+       
+        <Navbar/>
+        <Products products={products}/>
       </div>
     </Router>
   );
